5e479b0a9c39210c676376a2634d344affc1b041
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-12-11 19:56:10 -0300

    :sparkles: feat: create global audio context

---
+++ src/context/GlobalAudioContext.tsx
@@ -0,0 +1,48 @@
+import React, {
+  createContext,
+  Dispatch,
+  MutableRefObject,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+
+interface GlobalAudioContextType {
+  globalVolume: number;
+  audios: MutableRefObject<HTMLAudioElement | null>[];
+  addAudio: (audioRef: MutableRefObject<HTMLAudioElement | null>) => void;
+  setGlobalVolume: Dispatch<SetStateAction<number>>;
+}
+
+const GlobalAudioContext = createContext<GlobalAudioContextType | null>(null);
+
+const GlobalAudioProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [globalVolume, setGlobalVolume] = useState<number>(0.5);
+  const [audios, setAudios] = useState<
+    MutableRefObject<HTMLAudioElement | null>[]
+  >([]);
+
+  const addAudio = (audioRef: MutableRefObject<HTMLAudioElement | null>) => {
+    setAudios((prevState) => [...prevState, audioRef]);
+  };
+
+  return (
+    <GlobalAudioContext.Provider
+      value={{ globalVolume, audios, addAudio, setGlobalVolume }}
+    >
+      {children}
+    </GlobalAudioContext.Provider>
+  );
+};
+
+const useGlobalAudio = () => {
+  const context = useContext(GlobalAudioContext);
+  if (!context) {
+    throw new Error("useGlobalAudio must be used within a GlobalAudioProvider");
+  }
+  return context;
+};
+
+export { GlobalAudioProvider, useGlobalAudio };
