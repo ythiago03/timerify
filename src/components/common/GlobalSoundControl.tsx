ee888e13e127c73e61407d3566e9c51813ba4672
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-12-11 19:56:30 -0300

    :sparkles: feat: create global audio control component

---
+++ src/components/common/GlobalSoundControl.tsx
@@ -0,0 +1,34 @@
+import { useGlobalAudio } from "@/context/GlobalAudioContext";
+import { Volume1, Volume2, VolumeX } from "lucide-react";
+import { Slider } from "../ui/slider";
+
+const GlobalSoundControl: React.FC = () => {
+  const { globalVolume, setGlobalVolume, audios } = useGlobalAudio();
+
+  const handleMainVolume = (volume: number[]) => {
+    setGlobalVolume(volume[0] / 100);
+
+    audios.forEach((audioRef) => {
+      const audio = audioRef.current;
+      if (!audio) return;
+
+      audio.volume = globalVolume;
+    });
+  };
+  return (
+    <>
+      <Slider
+        onValueChange={handleMainVolume}
+        defaultValue={[50]}
+        max={100}
+        step={1}
+        className="w-24"
+      />
+      {globalVolume * 100 === 0 && <VolumeX />}
+      {globalVolume * 100 > 0 && globalVolume * 100 <= 50 && <Volume1 />}
+      {globalVolume * 100 > 0 && globalVolume * 100 > 50 && <Volume2 />}
+    </>
+  );
+};
+
+export default GlobalSoundControl;
