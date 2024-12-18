b23df4c1dcd5b5885c4ee202f6a3ca3cfa518fe3
Thiago Henrique <thiagohcfidencio@gmail.com>
2024-12-18 17:30:45 -0300

    :sparkles: feat: (Header.tsx) create header component

---
+++ src/components/template/Header.tsx
@@ -0,0 +1,47 @@
+import { Palette, Settings } from "lucide-react";
+import GlobalSoundControl from "../common/GlobalSoundControl";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+import Themes from "./Themes";
+
+interface HeaderProps {
+  toggleTheme: (theme: string, background?: string) => void;
+}
+
+const Header = ({ toggleTheme }: HeaderProps) => {
+  return (
+    <header className="w-full flex justify-center p-3">
+      <div className="flex w-1/2 justify-between items-center ">
+        <h1 className="text-3xl font-bold">Timerify</h1>
+        <ul className="flex gap-3">
+          <li className="flex gap-5">
+            <GlobalSoundControl />
+          </li>
+          <li>
+            <Sheet>
+              <SheetTrigger>
+                <Palette />
+              </SheetTrigger>
+              <SheetContent>
+                <SheetHeader>
+                  <SheetTitle>Choose your theme</SheetTitle>
+                  <Themes toggleTheme={toggleTheme} />
+                </SheetHeader>
+              </SheetContent>
+            </Sheet>
+          </li>
+          <li>
+            <Settings />
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
