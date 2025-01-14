da5694c1d5f90214745baccbdbaadf39b74c2440
Thiago Henrique Campos Fidêncio <thiagohcfidencio@gmail.com>
2025-01-14 20:01:01 -0300

    :hammer: refactor (GlobalAudioContext.tsx): fix type erros

--- src/context/GlobalAudioContext.tsx
+++ src/context/GlobalAudioContext.tsx
@@ -1,48 +1,48 @@
-import React, {
-  createContext,
-  Dispatch,
-  MutableRefObject,
-  SetStateAction,
-  useContext,
-  useState,
+import {
+	createContext,
+	type Dispatch,
+	type MutableRefObject,
+	type SetStateAction,
+	useContext,
+	useState,
 } from "react";
 
 interface GlobalAudioContextType {
-  globalVolume: number;
-  audios: MutableRefObject<HTMLAudioElement | null>[];
-  addAudio: (audioRef: MutableRefObject<HTMLAudioElement | null>) => void;
-  setGlobalVolume: Dispatch<SetStateAction<number>>;
+	globalVolume: number;
+	audios: MutableRefObject<HTMLAudioElement | null>[];
+	addAudio: (audioRef: MutableRefObject<HTMLAudioElement | null>) => void;
+	setGlobalVolume: Dispatch<SetStateAction<number>>;
 }
 
 const GlobalAudioContext = createContext<GlobalAudioContextType | null>(null);
 
 const GlobalAudioProvider: React.FC<{ children: React.ReactNode }> = ({
-  children,
+	children,
 }) => {
-  const [globalVolume, setGlobalVolume] = useState<number>(0.5);
-  const [audios, setAudios] = useState<
-    MutableRefObject<HTMLAudioElement | null>[]
-  >([]);
+	const [globalVolume, setGlobalVolume] = useState<number>(0.5);
+	const [audios, setAudios] = useState<
+		MutableRefObject<HTMLAudioElement | null>[]
+	>([]);
 
-  const addAudio = (audioRef: MutableRefObject<HTMLAudioElement | null>) => {
-    setAudios((prevState) => [...prevState, audioRef]);
-  };
+	const addAudio = (audioRef: MutableRefObject<HTMLAudioElement | null>) => {
+		setAudios((prevState) => [...prevState, audioRef]);
+	};
 
-  return (
-    <GlobalAudioContext.Provider
-      value={{ globalVolume, audios, addAudio, setGlobalVolume }}
-    >
-      {children}
-    </GlobalAudioContext.Provider>
-  );
+	return (
+		<GlobalAudioContext.Provider
+			value={{ globalVolume, audios, addAudio, setGlobalVolume }}
+		>
+			{children}
+		</GlobalAudioContext.Provider>
+	);
 };
 
 const useGlobalAudio = () => {
-  const context = useContext(GlobalAudioContext);
-  if (!context) {
-    throw new Error("useGlobalAudio must be used within a GlobalAudioProvider");
-  }
-  return context;
+	const context = useContext(GlobalAudioContext);
+	if (!context) {
+		throw new Error("useGlobalAudio must be used within a GlobalAudioProvider");
+	}
+	return context;
 };
 
 export { GlobalAudioProvider, useGlobalAudio };
